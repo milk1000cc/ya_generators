dabb5e0ee4b906235d7e7f67ee7c20d33b942538
milk1000cc <info@milk1000.cc>
2010-07-08 12:00:30 +0900

    use shoulda's macro for belongs_to's test

--- generators/ya_model/templates/model_spec.rb
+++ generators/ya_model/templates/model_spec.rb
@@ -2,12 +2,10 @@
 require 'spec_helper'
 
 describe <%= class_name %> do
-  <%- attributes.find_all { |v| v.reference? }.each do |a| -%>
-  it 'は、特定の <%= a.name.classify %> に所属すること' do
-    <%= a.name %> = Factory(:<%= a.name %>)
-    <%= singular_name %> = Factory(:<%= singular_name %>, :<%= a.name %>_id => <%= a.name %>.id)
-    <%= singular_name %>.<%= a.name %>.should == <%= a.name %>
-  end
+  <%- if attributes.any? { |v| v.reference? } -%>
+    <%- attributes.find_all { |v| v.reference? }.each do |a| -%>
+  it { should belong_to(:<%= a.name %>) }
+    <%- end -%>
 
   <%- end -%>
   describe 'を作成するとき' do
